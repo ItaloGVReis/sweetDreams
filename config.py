7ce3d48662a7d3a72348bcff5af1adb4be4c581c
Italo Gabriel <italogvr32@gmail.com>
2025-01-19 13:18:14 -0300

    d

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ load_dotenv()
 
 class Config:
     # Carregar variáveis de ambiente
-    MYSQL_HOST = os.getenv("MYSQL_HOST")  # Conexão interna (privada) do Railway
-    MYSQL_USER = os.getenv("MYSQL_USER")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-    MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
-    MYSQL_PORT = int(os.getenv("MYSQL_PORT"))
+    MYSQL_HOST = os.getenv("MYSQLHOST")  # Conexão interna (privada) do Railway
+    MYSQL_USER = os.getenv("MYSQLUSER")
+    MYSQL_PASSWORD = os.getenv("MYSQLPASSWORD")
+    MYSQL_DATABASE = os.getenv("MYSQLDATABASE")
+    MYSQL_PORT = int(os.getenv("MYSQLPORT"))
     
     # URL para a conexão MySQL (usar MYSQL_URL para a conexão completa)
     MYSQL_URI = f"mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
